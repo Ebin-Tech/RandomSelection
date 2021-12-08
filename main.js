6d8202e3b1dfb9a5acb68511d5785aa3cb2c9ddb
Ebin Benny Thomas <82931172+Ebin-Tech@users.noreply.github.com>
2021-12-08 18:34:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ let confettis = document.querySelector('#my-canvas');
 
 btn.addEventListener('click', add);
 function add() {
-    confettis.classList.add('active');
     const students =["Ebin","Neethu","Nidhi","Ashna","Nivya","Sreelakshmi","Sonu","Sonia","Athulya","Soorya","Athira","Roopak","sofy","Ronald","Robin","Rebin",];
     var random =Math.floor(16*Math.random());
     popup.classList.add('active');
@@ -15,12 +14,7 @@ function add() {
 close.addEventListener('click', remove);
 function remove() {
     popup.classList.remove('active');
-    confettis.classList.remove('active');
 }
-var confettiElement = document.getElementById('my-canvas');
-var confettiSettings = { target: confettiElement };
-var confetti = new ConfettiGenerator(confettiSettings);
-confetti.render();
 
 
 
